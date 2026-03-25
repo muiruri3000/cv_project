dab19b3c257f200b82e94c82380d8c754688b95d
muiruri3000 <josee.muiruri95@gmail.com>
2026-03-25 12:27:38 +0300

    changed from db to localhost

--- cv_backend/config/settings.py
+++ cv_backend/config/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB", "cv_db"),
         "USER": os.environ.get("POSTGRES_USER", "cv_user"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "cv_pass"),
-        "HOST": os.environ.get("POSTGRES_HOST", "db"), 
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"), 
         # db should be changed to localhost if running locally and not in container
         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
